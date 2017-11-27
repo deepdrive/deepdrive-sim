78b457ea04f9bef826a825b20900a7c6113ba0de
Craig Quiter <cquiter@gmail.com>
2017-11-26 22:20:11 -0700

    Remove extraneous prints

--- Plugins/DeepDrivePlugin/Source/DeepDrivePython/travis/get_patch_timestamp.py
+++ Plugins/DeepDrivePlugin/Source/DeepDrivePython/travis/get_patch_timestamp.py
@@ -3,13 +3,10 @@ from datetime import datetime, timedelta
 
 
 def git_commit_time_parse(t):
-    print(t)
     ret = datetime.strptime(t[0:19], '%Y-%m-%d %H:%M:%S')
     if t[20] == '+':
-        print('add')
         ret -= timedelta(hours=int(t[21:23]), minutes=int(t[23:25]))
     elif t[20] == '-':
-        print('sub')
         ret += timedelta(hours=int(t[21:23]), minutes=int(t[23:25]))
     return ret
 
