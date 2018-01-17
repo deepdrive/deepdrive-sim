e7e4e27ea5e53cf021b44a4c2598177d7263999f
Craig Quiter <cquiter@gmail.com>
2018-01-17 16:55:40 -0700

    Version bump, consolidate upload scripts

--- Packaging/upload_s3.py
+++ Packaging/upload_s3.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import os
+import platform
 import shutil
 import sys
 import tempfile
@@ -14,9 +15,9 @@ DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 def main():
-    out_filename = 'deepdrive-sim-windows-%s' % get_package_version()
+    out_filename = 'deepdrive-sim-%s-%s' % (platform.system().lower(), get_package_version())
     out_path = os.path.join(tempfile.gettempdir(), out_filename)
-    in_path = os.path.join(os.path.expanduser('~'), 'deepdrive-packaged')
+    in_path = os.path.join(os.path.expanduser('~'), 'DeepDrive')
     print('Zipping %s to %s.zip...' % (in_path, out_path))
     shutil.make_archive(out_path, 'zip', in_path)
     out_path += '.zip'
