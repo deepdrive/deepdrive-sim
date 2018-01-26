13aea654ef06cb0ed965f463b60aa9306d0a8e3b
Craig Quiter <cquiter@gmail.com>
2018-01-25 18:04:47 -0700

    Don't remove Saved folder in clean (has CPU background setting)

--- clean.bat
+++ clean.bat
@@ -1,7 +1,6 @@
 echo Cleaning derivative data
 
 RMDIR DerivedDataCache /S /Q
-RMDIR Saved /S /Q
 RMDIR Intermediate /S /Q
 RMDIR Binaries /S /Q
 RMDIR Plugins\DeepDrivePlugin\Binaries /S /Q
--- clean.sh
+++ clean.sh
@@ -9,7 +9,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo Cleaning derivative data from ${DIR}
 
 rm -rf ${DIR}/DerivedDataCache
-rm -rf ${DIR}/Saved
 rm -rf ${DIR}/Intermediate
 rm -rf ${DIR}/Binaries
 rm -rf ${DIR}/Plugins/DeepDrivePlugin/Binaries
