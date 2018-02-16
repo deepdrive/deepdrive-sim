f98c864e52b3b54b4f66d1793738c9c3de918c72
Craig Quiter <cquiter@gmail.com>
2018-02-16 13:33:14 -0700

    Uninstall external deepdrive package if possible

--- Plugins/DeepDrivePlugin/Source/DeepDrivePython/build/build.py
+++ Plugins/DeepDrivePlugin/Source/DeepDrivePython/build/build.py
@@ -45,6 +45,10 @@ def main(build_type):
     if build_type == 'dev':
         run_command('%s -m pip uninstall --yes '    % py + config.PACKAGE_NAME, env=env, cwd=ext_root)
         try:
+            run_command('pip uninstall --yes ' + config.PACKAGE_NAME, env=env, cwd=ext_root)
+        except Exception as e:
+            print('Best effort uninstall of external deepdrive package failed, error was: %s' % str(e))
+        try:
             run_command('%s -m pip install -e . --upgrade --force-reinstall --ignore-installed --no-deps' % py,
                     env=env, cwd=ext_root)
         except Exception as e:
