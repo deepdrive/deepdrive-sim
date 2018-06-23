e9a49726bf746b646aad9ab13b54b830dfa1014a
Maik Ernst <m.ernst@darknet-solutions.de>
2018-06-23 11:39:22 +0200

    - bugfix in PullOutState: desired speed wasn't calculated properly - adjusting some overtaking values (look ahead time)

--- Plugins/DeepDrivePlugin/Source/DeepDrivePlugin/Private/Simulation/Agent/Controllers/LocalAI/States/DeepDriveAgentPullOutState.cpp
+++ Plugins/DeepDrivePlugin/Source/DeepDrivePlugin/Private/Simulation/Agent/Controllers/LocalAI/States/DeepDriveAgentPullOutState.cpp
@@ -31,7 +31,8 @@ void DeepDriveAgentPullOutState::update(DeepDriveAgentLocalAIStateMachineContext
 	m_PullOutAlpha += m_PullOutTimeFactor * dT;
 	m_curOffset = FMath::Lerp(0.0f, ctx.configuration.OvertakingOffset, m_PullOutAlpha);
 
-	float desiredSpeed = FMath::Lerp(ctx.agent.getSpeedKmh(), desiredSpeed, m_PullOutAlpha);
+	float desiredSpeed = FMath::Lerp(ctx.agent.getSpeedKmh(), ctx.configuration.OvertakingSpeed, m_PullOutAlpha);
+	UE_LOG(LogDeepDriveAgentLocalAIController, Log, TEXT("Desired Speed %3.1f"), desiredSpeed);
 
 	if (m_PullOutAlpha >= 1.0f)
 	{
