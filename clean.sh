e2c8e60d4cd6220920d904179ae39cc6a6935b1a
Craig Quiter <cquiter@gmail.com>
2018-09-23 22:19:30 -0700

    ``

--- clean.sh
+++ clean.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
 set -e
-set -v
+
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
 echo Cleaning derivative data from ${DIR}
 
+set -v
 rm -rf ${DIR}/DerivedDataCache
 rm -rf ${DIR}/Intermediate
 rm -rf ${DIR}/Binaries
 rm -rf ${DIR}/Plugins/DeepDrivePlugin/Binaries
 rm -rf ${DIR}/Plugins/DeepDrivePlugin/Intermediate
+set +v
+
+echo Clean as a whistle!
