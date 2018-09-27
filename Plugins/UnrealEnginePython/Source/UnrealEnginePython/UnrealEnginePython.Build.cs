aed2dd81dd6f3a89931986f3fcb4585fc43a919d
Maik Ernst <m.ernst@darknet-solutions.de>
2018-09-27 22:38:03 +0200

    Revert "- local windows python path"
    This reverts commit f8b8f8081adeb1619a9bd5f534566b9dab58e898.

--- Plugins/UnrealEnginePython/Source/UnrealEnginePython/UnrealEnginePython.Build.cs
+++ Plugins/UnrealEnginePython/Source/UnrealEnginePython/UnrealEnginePython.Build.cs
@@ -21,8 +21,7 @@ public class UnrealEnginePython : ModuleRules
         "C:/Program Files/Python36",
         "C:/Program Files/Python35",
         "C:/Python27",
-        "C:/IntelPython35",
-        "C:/Users/Bender/AppData/Local/Programs/Python/Python35",
+        "C:/IntelPython35"
     };
 
     private string[] macKnownPaths =
