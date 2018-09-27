8975baca2bbe28f9e1c02cd0eef2df61334a499f
Craig Quiter <cquiter@gmail.com>
2018-09-26 22:25:04 -0700

    Detect embedded Python dependencies

--- Content/Scripts/ue_site.py
+++ Content/Scripts/ue_site.py
@@ -2,5 +2,24 @@
 On Editor/Engine start, the ue_site module is tried for import. You should place initialization code there.
 If the module cannot be imported, you will get a (harmful) message in the logs.
 
-This file is also necessary for pacakged builds
+This file is also necessary for packaged builds
 """
+from pathlib import Path
+
+try:
+    import zmq
+    import pyarrow
+    START_UNREAL_API_SERVER = True
+except ImportError:
+    START_UNREAL_API_SERVER = False
+    CURR_PATH = Path(__file__).resolve().parent
+    print('To enable the UnrealPython API, start the sim through the deepdrive project (github.com/deepdrive/deepdrive)'
+          ' and enter %s as the simulator project directory ' % CURR_PATH)
+
+
+def start_unreal_api_server():
+    pass
+
+
+if START_UNREAL_API_SERVER:
+    start_unreal_api_server()
