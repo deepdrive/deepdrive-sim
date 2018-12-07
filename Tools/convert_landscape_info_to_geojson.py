e4bff1b8fd55a6ea3d831177e3f2ac21bf433af5
Craig Quiter <cquiter@gmail.com>
2018-12-06 18:25:12 -0700

    Add turn radius (1/c), adjacent lanes, right/left lines, and output geojson format

--- Tools/convert_landscape_info_to_geojson.py
+++ Tools/convert_landscape_info_to_geojson.py
@@ -2,16 +2,22 @@ import copy
 import json
 import collections
 import os
-
+import uuid
 import numpy as np
 from scipy import spatial
 from scipy.interpolate import interp1d, BPoly
+
+
+
 
 GAP_CM = 100
 USE_TANGENTS = False
 CANYONS_FILENAME = 'landscape_segments.json'
 MESA_FILENAME = 'landscape_segments_mesa.json'
-
+# LANDSCAPE_OFFSET = np.array([-11200.000000, -11200.000000, 100.000000])  # Mesa offset
+LANDSCAPE_OFFSET = np.array([-21214.687500, -21041.564453, 18179.121094])  # Canyons TODO: Get this from Unreal
+LANDSCAPE_Z_SCALE = np.array([1, 1, 159.939941 / 100])
+INTERSECTIONS = False
 
 class HashableSegment(collections.MutableMapping):
     def __init__(self, segment):
@@ -37,6 +43,78 @@ class HashableSegment(collections.MutableMapping):
         return hash(self.store['full_name'])
 
 
+def get_graphs_by_side_world(graphs_by_side, landscape_offset, landscape_z_scale):
+    ret = {}
+    for side in graphs_by_side:
+        graph = graphs_by_side[side]
+        new_graph = {}
+        for p in graph:
+            p_world = landscape_to_world(landscape_offset, landscape_z_scale, p)
+            new_connections = []
+            for conn in graph[p]:
+                conn_world = landscape_to_world(landscape_offset, landscape_z_scale, conn)
+                new_connections.append(tuple(conn_world))
+            new_graph[tuple(p_world)] = new_connections
+        ret[side] = new_graph
+    return ret
+
+
+def add_point_relations(lane):
+    n = len(lane)
+    if n < 3:
+        return
+    for idx, segment in enumerate(lane):
+
+        A = np.array(lane[idx-1]['drive'])
+        B = np.array(segment['drive'])
+        C = np.array(lane[(idx+1) % n]['drive'])
+        a = np.linalg.norm(C - B)
+        b = np.linalg.norm(C - A)
+        c = np.linalg.norm(B - A)
+        s = (a + b + c) / 2
+        R = a * b * c / 4 / np.sqrt(s * (s - a) * (s - b) * (s - c))
+        print('radius %r' % R)
+        segment['radius'] = R
+        v = B - A
+        segment['direction'] = np.arctan(v[1] / v[0])
+        segment['distance'] = c
+        print('distance %r' % c)
+        if c > 500:
+            raise ValueError('Distance too large %r, A: %r B: %r' % (c, A, B))
+
+
+def add_ids(inner_lane, outer_lane):
+    if INTERSECTIONS:
+        raise NotImplementedError('Intersections not supported')
+    if len(inner_lane) != len(outer_lane):
+        raise ValueError('Expected inner and outer lane lengths too match')
+
+    for i, inner_seg in enumerate(inner_lane):
+        outer_seg = outer_lane[i]
+        inner_seg_id = new_seg_id()
+        outer_seg_id = new_seg_id()
+        add_seg_ids(inner_seg, inner_seg_id, outer_seg_id)
+        add_seg_ids(outer_seg, outer_seg_id, inner_seg_id)
+
+    ret = inner_lane + outer_lane
+    for i, seg in enumerate(ret):
+        seg['id'] = i
+    return ret
+
+
+def add_seg_ids(seg, my_id, adj_id):
+    seg['adjacent_segments'] = [adj_id]
+    seg['segment_id'] = my_id
+
+
+def new_seg_id():
+    # 33 billion meters of road - with 1 / billion collision chance =
+    # 36**x = 33e18 = 12.54
+    ret = str(uuid.uuid4()).replace('-', '')[:13]
+    ret = '%s-%s' % (ret[:5], ret[5:])
+    return ret
+
+
 def main():
     with open('landscape_segments.json') as f:
         segments = json.load(f)
@@ -45,11 +123,24 @@ def main():
     points = list(segment_point_map.keys())
     graphs = get_map_graphs(points, segment_point_map)
     graphs_by_side = match_graphs_to_side(graphs)
+
+    # The following depends on three separate landscape splines for the road and two guard rails
     side_point_mapping = match_points_to_sides(graphs_by_side, points)
-    sides = interpolate_points_by_segment(points, segment_point_map, side_point_mapping)
-    inner_lanes, outer_lanes = get_lanes(sides, graphs_by_side)
+    sides = interpolate_points_by_segment(points, segment_point_map, side_point_mapping,
+                                          LANDSCAPE_OFFSET, LANDSCAPE_Z_SCALE)
+
+    graphs_by_side_world = get_graphs_by_side_world(graphs_by_side, LANDSCAPE_OFFSET, LANDSCAPE_Z_SCALE)
+
+    inner_lane, outer_lane = get_lanes(sides, graphs_by_side_world)
+    # TODO: Go along drive paths and compute curvature with three consecutive points with 1 / radius, where r is given by https://stackoverflow.com/a/20315063/134077
+
+    add_point_relations(inner_lane)
+    add_point_relations(outer_lane)
+
+    all_lane_segments = add_ids(inner_lane, outer_lane)
+
     with open('deepdrive-canyons-map.json', 'w') as outfile:
-        json.dump(serialize_as_geojson(segments), outfile)
+        json.dump(serialize_as_geojson(all_lane_segments), outfile)
         print('Saved map to %s' % os.path.realpath(outfile.name))
 
 
@@ -60,74 +151,96 @@ def get_closest_point(point, kd_tree):
 
 
 def get_lanes(sides, graphs_by_side):
-    inner_lanes = []
-    outer_lanes = []
+    inner_lane = []
+    outer_lane = []
     start_point = (-27059.478515625, 22658.291015750001, 20553.831906106912)  #  (-5844.791015625, 43699.85546875, 1484.7515869140625)
-    second_point = (-26989.210175210432, 22727.784224634106, 20537.975077496125) #  (-1131.97998046875, 52382.1171875, 613.6241455078125)
-    end_point = (-27128.758053206424, 22593.083229395124, 20568.862235249715)  # (-8673.3046875, 42718.24609375, 1666.61572265625)
+    end_point = None  # (-8673.3046875, 42718.24609375, 1666.61572265625)
     center = start_point
-    prev = end_point
-    prev_inner = None
-    prev_outer = None
-    visited_inner = set()
-    visited_outer = set()
-    visited_center = set(start_point)
+    prev = (-27128.758053206424, 22593.083229395124, 20568.862235249715)
+    prev_drive_inner = None
+    prev_drive_outer = None
     inner_kd_tree = spatial.KDTree(sides['inner'])
     outer_kd_tree = spatial.KDTree(sides['outer'])
+    center_kd_tree = spatial.KDTree(sides['center'])
+    centers = sides['center']
+    center_idxs = get_center_idxs(centers)
     while True:
         inner, inner_dist = get_closest_point(center, inner_kd_tree)
         outer, outer_dist = get_closest_point(center, outer_kd_tree)
         check_lane_width(max(inner_dist, outer_dist))
-        if prev_inner is None or prev_outer is None:
-            print('Boostrapping previous points')
-        else:
-            # TODO: Add adjacent lane references
-            inner_lane = get_lane(center, inner, prev_inner, visited_inner, is_opposite=False)
-            outer_lane = get_lane(center, outer, prev_outer, visited_outer, is_opposite=True)
-            inner_lanes.append(inner_lane)
-            outer_lanes.append(outer_lane)
-        prev_inner, prev_outer = inner, outer
-        center = get_next_center(center, graphs_by_side, prev)
-        if center == start_point:
+        inner_lane_segment = get_lane(center, inner, prev_drive_inner, is_opposite=False)
+        outer_lane_segment = get_lane(center, outer, prev_drive_outer, is_opposite=True)
+        center, prev = get_next_center(center, graphs_by_side, prev, center_kd_tree), center
+        if end_point is not None and np.array_equal(center, end_point):
             break
-    return inner_lanes, outer_lanes
+        if prev_drive_inner is None or prev_drive_outer is None:
+            print('Bootstrapping prev inner and outer')
+            end_point = center
+        else:
+            inner_lane.append(inner_lane_segment)
+            outer_lane.append(outer_lane_segment)
+        prev_drive_inner = inner_lane_segment['drive']
+        prev_drive_outer = outer_lane_segment['drive']
+    return inner_lane, outer_lane
 
 
-def get_next_center(center, graphs_by_side, prev):
-    for neighbor in graphs_by_side['center'][center]:
-        if neighbor != prev:
-            return neighbor
+def get_center_idxs(centers):
+    center_idxs = collections.defaultdict(list)
+    for i, p in enumerate(centers):
+        center_idxs[tuple(p)].append(i)
+    return center_idxs
+
+
+def get_next_center(center, graphs_by_side, prev, kd_tree):
+    seg_end_points = graphs_by_side['center'].get(tuple(center), None)
+    if INTERSECTIONS and seg_end_points is not None:
+        # TODO: Actually don't do this for intersections. Just do BFS on the interpolated graph segments.
+
+        # Get the next segment. Same as getting next interpolated point when there are no intersections,
+        # but I don't want to fail silently if there are intersections.
+        if len(seg_end_points) > 2:
+            raise ValueError('Intersections and forks not yet supported')
+        else:
+            return [p for p in graphs_by_side['center'][center] if p != center][0]
     else:
-        raise ValueError('Could not get next center point')
+        # Grab the nearest interpolated point not equal to prev
+        results = kd_tree.query(np.array(center), 256)
+        for i, d in enumerate(results[0]):
+            if d == 0:
+                continue
+            p = kd_tree.data[results[1][i]]
+            if not np.array_equal(prev, p):
+                return tuple(p)
+        else:
+            raise ValueError('Could not find next center')
+
 
 def check_lane_width(dist):
-    if dist > 15 * 100:
+    if dist > 20 * 100:
         raise ValueError('Got lane width way wider than expected')
 
 
-def get_lane(left, right, prev, visited, is_opposite):
-    if right in visited:
-        raise ValueError('Right side of lane point is the closest point to two center points')
+def get_lane(left, right, prev, is_opposite):
+    left = np.array(left)
     drive = left + (right - left) * 0.5
-    if is_opposite:
-        vector = prev - right
+    if prev is None:
+        direction = None
     else:
-        vector = right - prev
-    direction = np.arctan(vector[1] / vector[0])
+        if is_opposite:
+            vector = prev - right
+        else:
+            vector = right - prev
+        direction = np.arctan(vector[1] / vector[0])
     lane = {'left': left, 'drive': drive, 'right': right, 'direction': direction}
-    visited.add(right)
     return lane
 
 
-def interpolate_points_by_segment(points, segment_point_map, side_point_mapping):
+def interpolate_points_by_segment(points, segment_point_map, side_point_mapping, landscape_offset, landscape_z_scale):
     visited_segments = set()
-    # landscape_offset = np.array([-11200.000000, -11200.000000, 100.000000])  # Mesa offset
-    landscape_offset = np.array([-21214.687500, -21041.564453, 18179.121094])  # Canyons TODO: Get this from Unreal
-    landscape_z_scale = np.array([1, 1, 159.939941 / 100])
     sides = collections.defaultdict(list)
     for point in points:
         if point not in side_point_mapping:
-            print('Ignoring point %r - not on track and should be deleted')
+            print('Ignoring point %r - not on track and should be deleted' % (point,))
             continue
         for segment in segment_point_map[point]:
             if segment in visited_segments:
@@ -137,12 +250,15 @@ def interpolate_points_by_segment(points, segment_point_map, side_point_mapping)
             center_points = get_interpolated_points(landscape_offset, landscape_z_scale, point, segment, 'Center')
             segment['interp_center_points'] = center_points
             sides[side_point_mapping[point]].extend(center_points)  # Guard rails define inner and outer road edges
+
             # TODO: Eventually we need to start with the map and build the level
             #       OR if we want to extract the map from landscape splines, we can use the mesh name
             #       to know the lanes and directions, then do a raycast search for the edges of the road
             #       starting from the center (either straight down or along the normal to the plane formed
             #       by the segment), so we are not reliant on matching Unreal's interpolation and
             #       we don't have to correct for Unreal's width segment connection tangents and control point tangents.
+            #       For the canyons map, the cubic interpolation looks perfect. For the mesa test map, it doesn't.
+            #       So, their could be some design params that can help avoid raycasting everything.
 
     return sides
 
@@ -191,43 +307,66 @@ def serialize_as_geojson(in_segments):
         'roadblocks': {},
         'stoplines': {},
     }
-    lane_segments = []
-    out_segment_id = 0
-    for s in in_segments:
-        points = s['interp_center_points']
-        for i in range(len(points) - 1):
-            start = points[i]
-            end = points[i + 1]
-            vector = end - start
-            lane_segments.append(
-                get_geojson_lane_segment(end, out_segment_id, start, vector))
-            out_segment_id += 1
-    out['lanes'] = {'features': lane_segments, 'type': 'FeatureCollection'}
+    out_segments = []
+    for i in range(len(in_segments) - 1):
+        start = in_segments[i]
+        end = in_segments[i + 1]
+        out_segments.append(
+            get_geojson_lane_segment(end, start))
+    out['lanes'] = {'features': out_segments, 'type': 'FeatureCollection'}
     return out
 
 
-def get_geojson_lane_segment(end, segment_id, start, vector):
-    return {
-        'geometry': {'type': 'LineString', 'coordinates': [start.tolist(), end.tolist()]},
-        'properties': {
-            'distance': np.linalg.norm(vector),
-            'direction': np.arctan(vector[1] / vector[0]),
-            'vehicle_type': "VEHICLE",
-            'id': segment_id,
-            'intersections': [],
-            'lane_count': 1,
-            'lane_num': 1,
-            'lane_type': "STRAIGHT",
-            'left_line': {},
-            'next_lanes': [],
-            'other_attributes': [],
-            # 'polygon': {type: "Polygon", coordinates: [[-87.6406294199376, 115.43642618666914, 125.653404236],…]},
-            'prev_lanes': [],
-            'radius': 0,
-            # 'right_line': {, …},
-            'speed_limit': 60,
-        },
-        'type': "Feature"}
+def get_geojson_lane_segment(segment, next_segment):
+    # TODO: Convert all cm values to meters (right, left)
+    properties = {
+        'vehicle_type': "VEHICLE",
+        'intersections': [],
+        'lane_count': 1,
+        'lane_num': 1,
+        'lane_type': "STRAIGHT",
+        'next_lanes': [],
+        'other_attributes': [],
+        'prev_lanes': [],
+        'speed_limit': 60,
+        'radius': segment['radius'] / 100,
+        'distance': segment['distance'] / 100,
+        'id': segment['id'],
+        'segment_id': segment['segment_id'],
+        'adjacent_segments': segment['adjacent_segments'],
+    }
+    coords = get_geojson_coords(segment, next_segment, 'drive')
+    right_coords = get_geojson_coords(segment, next_segment, 'right')
+    left_coords = get_geojson_coords(segment, next_segment, 'left')
+    properties['right_line'] = get_geojson_line(right_coords)
+    properties['left_line'] = get_geojson_line(left_coords)
+    polygon = np.array([segment['left'], next_segment['left'], segment['right'], next_segment['right']]) / 100
+    properties['polygon'] = {'coordinates': polygon.tolist(), 'type': 'Polygon'}
+    ret = {
+        'geometry': {'type': 'LineString', 'coordinates': coords},
+        'properties': properties,
+        'type': 'Feature'}
+    return ret
+
+
+def get_geojson_line(left_coords):
+    return {'geometry': {'type': 'LineString', 'coordinates': left_coords}, 'line_color': 'WHITE',
+            'line_type': 'DOUBLE_SOLID_LINE'}
+
+
+def get_geojson_coords(segment, next_segment, side):
+    return (np.array([segment[side], next_segment[side]]) / 100).tolist()
+
+
+"""
+geometry: {type: "LineString", coordinates: [[-91.23195947978866, 115.201464914047, 125.591217041],…]}
+coordinates: [[-91.23195947978866, 115.201464914047, 125.591217041],…]
+0: [-91.23195947978866, 115.201464914047, 125.591217041]
+1: [-91.17320342247241, 114.30339182956251, 125.601303101]
+type: "LineString"
+line_color: "NONE"
+line_type: "IMPLIED"
+"""
 
 
 def get_interpolated_points(landscape_offset, landscape_z_scale, point, segment, side_name):
@@ -251,7 +390,7 @@ def get_interpolated_points(landscape_offset, landscape_z_scale, point, segment,
 def interpolate(landscape_offset, landscape_z_scale, segment, side):
     """
     :param landscape_offset: Center of landscape which Unreal uses as origin for segment point coordinates
-    :param landscape_offset: Z-scale of landscape in Unreal transform
+    :param landscape_z_scale: Z-scale of landscape in Unreal transform
     :param segment: Unreal segment
     :param side: Left, Center, or Right
     :return:
@@ -271,8 +410,8 @@ def interpolate(landscape_offset, landscape_z_scale, segment, side):
         points = np.array(points)
         total_points = distances[-1] // GAP_CM + 1
         chords = list(np.arange(0, total_points) * GAP_CM)
-        if distances[-1] % GAP_CM > (GAP_CM / 2):
-            # Add an extra interp point so there won't be a gap longer than meter
+        if distances[-1] % GAP_CM > (GAP_CM / 3):
+            # Add an extra interp point so there won't be big gap
             chords.append(distances[-1])
         cubic_interpolator = interp1d(distances, points, 'cubic', axis=0)
         ret = cubic_interpolator(chords)
