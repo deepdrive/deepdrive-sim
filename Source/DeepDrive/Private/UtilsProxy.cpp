d58ec00940ae036f850a29713e56a349139500b5
Craig Quiter <cquiter@gmail.com>
2018-12-10 16:59:22 -0700

    Comment out UtilsProxy to fix LandscapeInfo packaging error. TODO: Don't include editor dependencies in Runtime code.

--- Source/DeepDrive/Private/UtilsProxy.cpp
+++ Source/DeepDrive/Private/UtilsProxy.cpp
@@ -1,33 +1,33 @@
-#include "UtilsProxy.h"
-
-#include "Runtime/Landscape/Classes/LandscapeSplineSegment.h"
-
-//DEFINE_LOG_CATEGORY(LogDeepDriveUtilsProxy);
-
-// Sets default values
-AUtilsProxy::AUtilsProxy()
-{
- 	// Set this actor to call Tick() every frame. You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = false;
-}
-
-void AUtilsProxy::GetSplineSegmentConnections()
-{
-	UE_LOG(LogTemp, Log, TEXT("GetSplineSegmentConnections !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"));
-	//return StructProperty;
-}
-
-// Called when the game starts or when spawned
-void AUtilsProxy::BeginPlay()
-{
-	Super::BeginPlay();
-	UE_LOG(LogTemp, Log, TEXT("UtilsProxy started"));
-}
-
-// Called every frame
-void AUtilsProxy::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
+//#include "UtilsProxy.h"
+//
+////#include "Runtime/Landscape/Classes/LandscapeSplineSegment.h"
+//
+////DEFINE_LOG_CATEGORY(LogDeepDriveUtilsProxy);
+//
+//// Sets default values
+//AUtilsProxy::AUtilsProxy()
+//{
+// 	// Set this actor to call Tick() every frame. You can turn this off to improve performance if you don't need it.
+//	PrimaryActorTick.bCanEverTick = false;
+//}
+//
+//void AUtilsProxy::GetSplineSegmentConnections()
+//{
+//	UE_LOG(LogTemp, Log, TEXT("GetSplineSegmentConnections"));
+//	//return StructProperty;
+//}
+//
+//// Called when the game starts or when spawned
+//void AUtilsProxy::BeginPlay()
+//{
+//	Super::BeginPlay();
+//	UE_LOG(LogTemp, Log, TEXT("UtilsProxy started"));
+//}
+//
+//// Called every frame
+//void AUtilsProxy::Tick(float DeltaTime)
+//{
+//	Super::Tick(DeltaTime);
+//
+//}
 
