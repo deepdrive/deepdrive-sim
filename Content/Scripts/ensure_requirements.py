a54386f47f169e2002d0a7e05e0ffdaafc1cf507
Craig Quiter <cquiter@gmail.com>
2018-12-30 13:17:12 -0700

    Lazily install UEPy requirements

---
+++ Content/Scripts/ensure_requirements.py
@@ -0,0 +1,49 @@
+import os
+import sys
+import pip
+
+from pathlib import Path
+import importlib
+import traceback
+
+REQ_DIR_NAME = 'UEPyPackages'
+REQUIREMENTS = [
+    dict(module='zmq', pip='pyzmq'),
+    dict(module='pyarrow', pip='pyarrow')
+]
+
+
+def ensure_requirements():
+    curr_dir = os.path.dirname(get_this_filename())
+    req_dir = str(Path(curr_dir).parent.parent.joinpath(REQ_DIR_NAME))
+    if req_dir not in sys.path:
+        sys.path.insert(0, req_dir)
+    for req in REQUIREMENTS:
+        module = req['module']
+        pip_name = req['pip']
+        try:
+            importlib.import_module(module)
+        except ImportError:
+            print('Could not import %s. Installing to %s...' % (module, req_dir))
+            pip_install(pip_name, req_dir)
+        else:
+            print('Found %s' % pip_name)
+
+
+def pip_install(package, dirname):
+    if hasattr(pip, 'main'):
+        pip.main(['install', '--target', dirname, package])
+
+
+def get_this_filename():
+    # Hack to get current file path in UnrealEditor
+    try:
+        raise NotImplementedError("No error")
+    except Exception as e:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        filename = traceback.extract_tb(exc_traceback)[-1].filename
+    return filename
+
+
+if __name__ == '__main__':
+    ensure_requirements()
--- Content/Scripts/ue_site.py
+++ Content/Scripts/ue_site.py
@@ -5,39 +5,11 @@ If the module cannot be imported, you will get a (harmful) message in the logs.
 This file is also necessary for packaged builds
 """
 
-# TODO: Perhaps download python libs here with:
-# import os
-# # noinspection PyCompatibility
-# import urllib.request
-# from io import BytesIO
-# from zipfile import ZipFile
-# from pathlib import Path
-#
-# CURR_PATH = Path(__file__).resolve().parent
-# LIB_PATH = CURR_PATH.parent.parent / 'python_libs'
-# LIB_URL = 'https://s3-us-west-1.amazonaws.com/deepdrive/unreal_python_lib/python_libs.zip'
-#
-#
-# def main():
-#     if not LIB_PATH.exists() or next(LIB_PATH.iterdir(), None) is None:
-#         LIB_PATH.mkdir(exist_ok=True)
-#         print('Downloading Python libs (71MB) for Unreal embedded Python from', LIB_URL, '...')
-#         download_zip(LIB_URL, str(LIB_PATH))
-#
-#
-# def download_zip(url, path):
-#     """Download a zip file and unzipping it to path"""
-#     with urllib.request.urlopen(url) as response:
-#         content = response.read()
-#         zip_ref = ZipFile(BytesIO(content))
-#         try:
-#             zip_ref.extractall(path)
-#         except Exception:
-#             print('You may want to close all programs that may have these files open or delete existing '
-#                   'folders this is trying to overwrite')
-#             raise
-#         finally:
-#             zip_ref.close()
-#
-#
-# main()
+import ensure_requirements
+
+
+def main():
+    ensure_requirements.ensure_requirements()
+
+
+main()
