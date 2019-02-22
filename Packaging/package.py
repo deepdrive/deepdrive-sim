c0b5258557c6251a405fa87358570a8f00dbafce
Craig Quiter <cquiter@gmail.com>
2019-02-21 19:45:11 -0800

    Don't install bundled plugins during package

--- Packaging/package.py
+++ Packaging/package.py
@@ -7,26 +7,27 @@ root = dirname(dirname(abspath(__file__)))
 
 # TODO: download our plugin zip files from AWS if the local versions don't exist
 
-# Download and extract the prebuilt binaries for the Substance plugin
-print('Downloading and extracting the prebuilt Substance plugin...')
-UnrealUtils.install_plugin(join(root, 'Packaging', 'Substance-4.21.0.31-Desktop.zip'), 'Substance', prefix='Marketplace')
-
-# Download and extract the prebuilt binaries for the UnrealEnginePython plugin
-print('Downloading and extracting the prebuilt UnrealEnginePython plugin...')
-UnrealUtils.install_plugin(join(root, 'Packaging', 'UnrealEnginePython-20181229-Linux.zip'), 'UnrealEnginePython')
+# # Download and extract the prebuilt binaries for the Substance plugin
+# print('Downloading and extracting the prebuilt Substance plugin...')
+# UnrealUtils.install_plugin(join(root, 'Packaging', 'Substance-4.21.0.31-Desktop.zip'), 'Substance', prefix='Marketplace')
+#
+# # Download and extract the prebuilt binaries for  the UnrealEnginePython plugin
+# print('Downloading and extracting the prebuilt UnrealEnginePython plugin...')
+# UnrealUtils.install_plugin(join(root, 'Packaging', 'UnrealEnginePython-20190128-Linux.zip'), 'UnrealEnginePython')
 
 # Create our project packager
 packager = ProjectPackager(
-	root = root,
-	version = FilesystemUtils.read(join(root, 'Content', 'Data', 'VERSION')),
-	archive = '{name}-{version}-{platform}'
+    root=root,
+    version=FilesystemUtils.read(join(root, 'Content', 'Data', 'VERSION')),
+    archive='{name}-{version}-{platform}',
+
 )
 
 # Clean any previous build artifacts
 packager.clean()
 
 # Package the project
-packager.package()
+packager.package(args=['Development'])
 
 # Compress the packaged distribution
 archive = packager.archive()
