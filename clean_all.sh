dfd8d2abc74a78a0c5c55c4df994ec0b80f5dc14
Craig Quiter <cquiter@gmail.com>
2019-02-24 14:04:00 -0700

    Add CMakeLists.txt and code completion files to clean_all.sh

--- clean_all.sh
+++ clean_all.sh
@@ -11,9 +11,24 @@ echo Cleaning derivative data from ${DIR}
 set -v
 rm -rf ${DIR}/DerivedDataCache
 rm -rf ${DIR}/Intermediate
+rm -rf ${DIR}/Build
 rm -rf ${DIR}/Binaries
 rm -rf ${DIR}/Plugins/DeepDrivePlugin/Binaries
 rm -rf ${DIR}/Plugins/DeepDrivePlugin/Intermediate
+rm -rf ${DIR}/.kdev4
+rm -rf ${DIR}/.vscode
+rm -rf ${DIR}/CMakeLists.txt
+rm -rf ${DIR}/DeepDrive.code-workspace
+rm -rf ${DIR}/DeepDrive.kdev4
+rm -rf ${DIR}/DeepDrive.pro
+rm -rf ${DIR}/DeepDrive.workspace
+rm -rf ${DIR}/DeepDriveConfig.pri
+rm -rf ${DIR}/DeepDriveDefines.pri
+rm -rf ${DIR}/DeepDriveHeader.pri
+rm -rf ${DIR}/DeepDriveIncludes.pri
+rm -rf ${DIR}/DeepDriveSource.pri
+rm -rf ${DIR}/DeepDriveCodeLitePreProcessor.txt
+rm -rf ${DIR}/DeepDriveCodeCompletionFolders.txt
 
 # In case this is executed with cygwin, don't delete python binaries - just UEPy ones
 find ${DIR}/Plugins/UnrealEnginePython/Binaries -type f -iname '*unreal*' -delete
