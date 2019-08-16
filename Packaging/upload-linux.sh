4c198c5a17b7e3b24964d9b1369372647e5e82a3
Craig Quiter <cquiter@gmail.com>
2019-08-16 15:18:24 -0700

    Upload the most recent sim, not the oldest!

--- Packaging/upload-linux.sh
+++ Packaging/upload-linux.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-zip_name=`ls -la deepdrive-sim-linux*|sort|fmt|tail -n 1`
+zip_name=`ls -la deepdrive-sim-linux*|sort -r|fmt|tail -n 1 | awk 'NF>1{print $NF}'`
 aws --profile=default s3 cp ${zip_name} s3://deepdrive/sim/${zip_name}
