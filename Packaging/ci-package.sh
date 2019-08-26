b496ebe83d60c2b99d686ee9b5908ecf2a318908
Craig Quiter <cquiter@gmail.com>
2019-08-26 14:10:22 -0700

    Don't run old upload-linux.sh

--- Packaging/ci-package.sh
+++ Packaging/ci-package.sh
@@ -4,5 +4,3 @@ git clone --depth=1 --branch ${DEEPDRIVE_BRANCH} https://github.com/deepdrive/de
 cd deepdrive-sim
 git checkout -qf ${DEEPDRIVE_COMMIT}
 python3 -u Packaging/package.py
-
-Packaging/upload-linux.sh
