06f200e2a8ee022613206e0d55d4c2d5955a05a1
Craig Quiter <cquiter@gmail.com>
2019-10-02 14:41:53 -0700

    Reinstate the REQUESTS_CA_BUNDLE var to fix ```2019-10-02T21:39:47.301644437Z OSError: Could not find a suitable TLS CA certificate bundle, invalid path: /home/ue4/.local/lib/python3.6/site-packages/certifi/cacert.pem```

--- Packaging/ci-package.sh
+++ Packaging/ci-package.sh
@@ -10,4 +10,7 @@ git clean -fxd
 git fetch origin ${DEEPDRIVE_BRANCH}
 git checkout -qf ${DEEPDRIVE_COMMIT}
 
+# https://stackoverflow.com/a/42982144/134077
+export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
+
 python3 -u Packaging/package.py --upload
