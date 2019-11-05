06133b80aa113bbd693e16ccb2a7876f73ccf82c
Craig Quiter <cquiter@gmail.com>
2019-11-05 10:27:46 -0800

    Get local packaging working with Kevindale

--- Packaging/package-linux.sh
+++ Packaging/package-linux.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
-docker run --rm -ti "-v$ROOTDIR:/hostdir" -e DEEPDRIVE_PACKAGE_NO_UPLOAD=1 -w /hostdir deepdriveio/ue4-deepdrive-deps:latest /hostdir/Packaging/in-docker-package.sh
+docker run --net=host -ti "-v$ROOTDIR:/hostdir" -v ~/.gcpcreds:/.gcpcreds  -e DEEPDRIVE_PACKAGE_NO_UPLOAD=1 -e GOOGLE_APPLICATION_CREDENTIALS="/.gcpcreds/VoyageProject-d33af8724280.json" -w /hostdir deepdriveio/ue4-deepdrive-deps:latest /hostdir/Packaging/in-docker-package.sh
